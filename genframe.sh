fd1c4406dc8c22e0eeec35b98dab9783913dc7e6
Rilder de Sousa Pires <rilder@bernoulli.intranet.fisica.ufc.br>
2011-02-11 15:18:31 -0300

    Beeman's algorithm was corrected to calculate the forces once, and the Cwall class was modified to represent walls in any direction.

--- genframe.sh
+++ genframe.sh
@@ -3,7 +3,7 @@ files=$*
 
 for f in $files
 do
-	./xbps -s1 -x0 -y0 $f > $f.ps
+	./xbps -s0.5 -x0 -y0 $f > $f.ps
 	convert $f.ps $f.jpg
 done
 	
--- main.cc
+++ main.cc
@@ -4,6 +4,8 @@
 #include<iomanip>
 #include"particle.h"
 
+#define sgn(a) ((a) > 0) ? (1.0) : (-1.0)
+
 using namespace std;
 
 double t=0;
@@ -19,7 +21,7 @@ size_t N=NL*NC;
 
 class CWall{
 	public:
-	CWall(double _x1=0, double _y1=0, double _x2=10, double _y2=0){
+	CWall(double _x1, double _y1, double _x2, double _y2){
 		x1=_x1;
 		x2=_x2;
 		y1=_y1;
@@ -27,22 +29,48 @@ class CWall{
 		}
 
 	void interact(CParticle &p){
+		double wl=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 
-		double d=p.y-y1;
-		if( d > p.r)return;
-		double ovl= fabs(d - p.r);
-		double vn=p.vy;
+		double tx=(x2-x1)/wl,ty=(y2-y1)/wl;
+		double nx=ty,ny=-tx;
+		double qx=(p.x-x1),qy=(p.y-y1);
+		int sg = 1;
 
-   		double fn=p.kn*pow(ovl,1.5);//-cn*vn; 
+		double l=(qx*tx + qy*ty);
+		double d;
 
-		p.fy+=fn;
+		if(l<0){
+			d=sqrt(qx*qx + qy*qy);
+			if( d > p.r)return;
+			nx=qx/d; ny=qy/d;
+		}else{
+
+			if(l>wl){
+				qx=(p.x-x2); qy=(p.y-y2);
+				d=sqrt(qx*qx + qy*qy);
+				if( d > p.r)return;
+				nx=qx/d; ny=qy/d;
+			}else{
+				d=fabs(qx*nx + qy*ny);
+				if( d > p.r)return;
+				sg=sgn(qx*nx + qy*ny);
+			};
+		};
+
+		double ovl=fabs(d - p.r);	//double vn=p.vy;
+   		double fn=sg*p.kn*pow(ovl,1.5);
+		p.fx+=fn*nx; p.fy+=fn*ny;
+
 		}
 
 	double x1, x2;
 	double y1, y2;
 	};
 
-CWall wall;
+CWall wall1(0,0,L,0);
+CWall wall2(0,0,0,L);
+CWall wall3(L,0,L,L);
+CWall wall4(0,L,L,L);
 
 double cal_energy(CParticle *p){
 
@@ -62,7 +90,11 @@ void cal_forces(CParticle p[]){
 		p[i].fx=0;
 		p[i].fy=G*p[i].m;
 
-		wall.interact(p[i]);
+		wall1.interact(p[i]);
+		wall2.interact(p[i]);
+		wall3.interact(p[i]);
+		wall4.interact(p[i]);
+
 		for(int j=0; j<i; j++){
 		p[i].interact(p[j]);
 
@@ -73,9 +105,6 @@ void cal_forces(CParticle p[]){
 void output(CParticle *p){
         static int count=0, outN=0,outPutN=outDt/dt;
         static ofstream out;
-        //static double Energy=0.0, rEnergy=0, pEnergy=0, kEnergy=0;
-
-
 
         if(count%outPutN==0 ){
                         stringstream outstream;
@@ -92,17 +121,12 @@ void output(CParticle *p){
                         count=0;
                         outN++;
 			cout<< t <<"\t"<< cal_energy(p) <<endl;
-
-                        //Energy=rEnergy+kEnergy+pEnergy;
-                        //outEnergy<<setprecision(14)<<t<<"  "<<Energy<<"  "<<kEnergy<<"  "<<pEnergy<<"  "<<rEnergy <<endl;
-                        //rEnergy=0; pEnergy=0; kEnergy=0; Energy=0;
-                        }
+                       }
 		count++;
 
 	}
 
 int main(){
-
 
 
 
@@ -115,20 +139,13 @@ double v0=10;
    double r=1;
    p[i*NC+j].r=r;
    p[i*NC+j].set_pos(2.0*r+j*(L-2.5*r)/NC +(i%2)*0.5*L/NC, 2.0*r + i*L/NC) ;
-   double theta=(rand()%100001)*6.28318e-5;
+   double theta=(rand()%100001)*M_PI*2.0e-5;
    p[i*NC+j].set_vel(v0*cos(theta), v0*sin(theta));
-//   p[i*NC+j].q=0;  
-//   p[i*NC+j].w=0;
   }; 
  };
 
 
 while (t<maxtime){
-
-	cal_forces(p);
-	for(int i=0; i<N; i++){
-		p[i].update_accel();
-	}
 
 	for(int i=0; i<N; i++){
 		p[i].predict(dt);
@@ -137,8 +154,7 @@ while (t<maxtime){
 	cal_forces(p);
 
 	for(int i=0; i<N; i++){
-		p[i].update_accel2();
-		//cerr<<p[i].m<<endl;
+		p[i].update_temp_accel();
 	}
 
 	for(int i=0; i<N; i++){
@@ -146,15 +162,12 @@ while (t<maxtime){
 		}
 
 	for(int i=0; i<N; i++){
-		p[i].update_accel3();
+		p[i].update_accel();
 	}
 
-//	cout<< t <<"\t"<< cal_energy(p) <<endl;
 	t+=dt;
-
 	output(p);
 	}
-
 
 return 0;
 }
